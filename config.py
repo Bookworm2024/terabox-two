54afe8fc84aee7df460c8870c224126c39906717
BookWorm <infohubnetworks@gmail.com>
2025-02-13 06:53:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 21145186  # api id
+API_HASH = "daa53f4216112ad22b8a8f6299936a46"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7510192674:AAGG3VDjcN7cgJhhhxHXtu1_il_mkbP4eRU"  # bot token
 
 
 # REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+HOST = "https://redis-19646.c278.us-east-1-4.ec2.redns.redis-cloud.com"  # redis host uri
+PORT = 19646  # redis port
+PASSWORD = "WrXIxdcQFeXBnaI9WIt3rurNeTgMKZ69"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002379300008  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
-ADMINS = [1317173146]
+COOKIE = "csrfToken=OcblaSuA76RERkNUGrMv64D5; browserid=19daDVky7HAOOc6EeQTYw058mrQwx3usmoSq3cY4ti1_dnnjGtyrzWLSmycjkxOdP7kQTeW2dNQVP86n; lang=en; __bid_n=194f7f908fc564e3e84207; PANWEB=1; _ga=GA1.1.1283407917.1739359467; _gcl_au=1.1.1954529936.1739359467; __stripe_sid=e0c2fda0-856a-43a5-abfa-52a3a52d0615be6ce3; _ga_K6JMPYL99R=GS1.1.1739359467.1.0.1739359531.0.0.0; ndut_fmt=83CD34FB93DE5A770A20AC41BAA34B4E006CA2ADFD3DAABE2605C2AEEF4036EB"
+ADMINS = [6011680723]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "teraboxdownloading_robot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@Bookslibraryofficial"
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
